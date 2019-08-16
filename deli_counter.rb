ef35908fa7644a57ae5cefc698a0f8e5a06b3e4b
Mars Le <hp.mars.le@gmail.com>
2019-08-16 04:03:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,13 +4,14 @@ katz_deli =[]
 def line(deli_line)
   if deli_line.length >= 1 
     
-  #shows their current place in line
-  "The line is currently: " 
-  deli_line.each_with_index do |name, i|
-    print "#{i+1}. #{name} "
-  end 
-else
-   puts "The line is currently empty"
+    #shows their current place in line
+    "The line is currently: " 
+    deli_line.each_with_index do |name, i|
+      print "#{i+1}. #{name} "
+    end 
+  else
+    puts "The line is currently empty"
+  end
 end
 
 # def take_a_number (deli_line, name)
