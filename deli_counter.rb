098e1d957c5f120f5d10789bb15815cf4a6ffda0
Mars Le <hp.mars.le@gmail.com>
2019-08-16 04:24:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,10 +13,10 @@ def line(deli_line)
   end
 end
 
-# def take_a_number (deli_line, name)
-#   #puts person's name along with their position in line
-#   #remember people count from 1 
-# end 
+def take_a_number (deli_line, name)
+  #puts person's name along with their position in line
+  #remember people count from 1 
+end 
 
 # def now_serving 
 #   # puts the next person in line and removes them from the 
