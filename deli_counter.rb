e79d429f8aeecce225b0f9d811b855269e36ebc8
Mars Le <hp.mars.le@gmail.com>
2019-08-16 03:35:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+katz_deli =[]
+
+def line 
+  #shows their current place in line
+  
+  #if noone is in line
+    "The line is currently empty"
+end
+
+def take_a_number (deli_line, name)
+  #puts person's name along with their position in line
+  #remember people count from 1 
+end 
+
+def now_serving 
+  # puts the next person in line and removes them from the 
+  #thinking shift
+  #if there's noone in line 
+    puts "There is nobody waiting to be served!"
+end 
